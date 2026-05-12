ee8a9b3945c76d4adb05f3a84e6707c251eeea63
wh1skyyyy <2751258052@qq.com>
2026-05-12 09:55:07 +0800

    Update app.py

--- app.py
+++ app.py
@@ -49,5 +49,6 @@ def health():
     return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    import os
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
