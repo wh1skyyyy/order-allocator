f25e442644c3c857ce0c6529a701101c66a2cef7
wh1skyyyy <2751258052@qq.com>
2026-05-12 09:53:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-﻿from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify
 from itertools import product
+import os
 
 app = Flask(__name__)
 
@@ -48,4 +49,5 @@ def health():
     return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
